38ba660379db5afeb4b593b1d22322dba0fe5735
Zintan Mwinila-Yuori <zmwinilayuori@cs.hmc.edu>
2020-09-27 16:35:03 -0700

    working on exercise 2

--- kern/env.c
+++ kern/env.c
@@ -116,7 +116,15 @@ env_init(void)
 {
 	// Set up envs array
 	// LAB 3: Your code here.
-
+	size_t i;
+	*env_free_list = envs[0];
+	for (i = 0; i < NENV; i++) {
+		envs[i].env_status = ENV_FREE;
+		envs[i].env_id = 0;
+		if (i < NENV - 1) {
+			envs[i].env_link = &envs[i+1];
+		}
+	}
 	// Per-CPU part of the initialization
 	env_init_percpu();
 }
@@ -179,7 +187,7 @@ env_setup_vm(struct Env *e)
 	//    - The functions in kern/pmap.h are handy.
 
 	// LAB 3: Your code here.
-
+	e->env_pgdir = page2kva(p);
 	// UVPT maps the env's own page table read-only.
 	// Permissions: kernel R, user R
 	e->env_pgdir[PDX(UVPT)] = PADDR(e->env_pgdir) | PTE_P | PTE_U;
