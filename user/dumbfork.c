d8bff999978d65c98f17ab59b4e43fc58f28e1c7
dmind44 <davidmindlin4@gmail.com>
2020-10-25 21:08:14 -0700

    removed extraneous print

--- user/dumbfork.c
+++ user/dumbfork.c
@@ -72,7 +72,6 @@ dumbfork(void)
 	duppage(envid, ROUNDDOWN(&addr, PGSIZE));
 
 	// Start the child environment running
-	cprintf("setting child status. \n");
 	if ((r = sys_env_set_status(envid, ENV_RUNNABLE)) < 0)
 		panic("sys_env_set_status: %e", r);
 
